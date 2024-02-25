cb2a9630aeafd36ffcb086e8ce95cc33b844c6bb
Meer Quais <firebase.fallen@gmail.com>
2024-02-25 15:37:00 +0500

    Class 01 of JavaScript

---
+++ JavaScript/class 01/app.js
@@ -0,0 +1,11 @@
+// alert("HELLO WORLD ");
+
+
+//=======Translator=====
+// compiler;
+// Interpreter;
+// assembler;
+
+// alert("Hello world 1");
+// alert("Hello World 3");
+// alert("Hello World 2");
