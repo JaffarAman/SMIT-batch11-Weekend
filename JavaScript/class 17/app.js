c916069110204b77db6d53be18d489388cfdbd11
Jaffar <amanjaffar50@gmail.com>
2024-05-05 09:11:57 +0500

    class 17

---
+++ JavaScript/class 17/app.js
@@ -0,0 +1,59 @@
+// console.log("para1", para1)
+
+// // var para1 = document.getElementById("para1")
+// var paras = document.getElementsByTagName("p")
+// console.log(paras)
+
+// var a = [0, 1, 2, 3, 4]
+// console.log(a[0, 4])
+
+// var allPara = document.getElementsByTagName("p")
+// console.log("allPara", allPara[2])
+
+// allPara[2].style.background = "red"
+
+
+// var paras = document.getElementsByTagName("p")
+// console.log("paras", paras)
+// for (var i = 0; i < paras.length; i++) {
+//     console.log("para", paras[i].style.color = "red");
+// }
+
+// console.log("paras", paras[4])
+// paras.style.color = "red"
+// paras[4].style.color = "red"
+// paras[4].style.background = "black"
+// paras[4].style.fontSize = "42px"
+
+
+// var paras = document.getElementsByTagName("p")
+// console.log("paras", paras)
+// var parent = document.getElementById("parent")
+// console.log("parent", parent)
+// var childParas = parent.getElementsByTagName("p")
+
+// for (var i = 0; i < childParas.length; i++) {
+//     childParas[i].style.color = "red"
+//     childParas[i].style.background = "green"
+// }
+// console.log("childParas", childParas)
+
+
+
+
+// var paras = document.getElementsByTagName("p")
+// for (var i = 0; i < paras.length; i++) {
+//     if (i % 2 !== 0) {
+//         paras[i].style.color = "red"
+//         paras[i].style.background = "green"
+//     }
+// }
+
+// var para = document.getElementById("para")
+// console.log("para", para.innerHTML)
+
+
+
+// function seemore() {
+//     para.innerHTML = "SAYLANI"
+// }
