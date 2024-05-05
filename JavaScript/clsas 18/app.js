73a95702b91f8c4fdebc1ed610f3f28943d89579
Jaffar <amanjaffar50@gmail.com>
2024-05-05 11:39:55 +0500

    revision js

---
+++ JavaScript/clsas 18/app.js
@@ -0,0 +1,163 @@
+// console.log("HELLO WORLD")
+
+// var
+// let
+// const
+
+// let userName = "Jaffar Aman";
+// let userName = "Meer";
+// console.log(userName, "userName")  
+
+
+// var userName = "Jaffar";
+// console.log(userName)
+
+// var userName = "Jaffar";
+// console.log(userName, "1 userName")
+
+// userName = "Jaffar Aman"
+
+// console.log(userName, "2 userName")
+
+
+// var name = "Jaffar"
+
+// // name = name + "Aman"
+
+// name += " Aman"
+// console.log(name)
+
+// primitive Data type
+var str = 'Jaffar' // string
+var num = 23 // number
+var bool = true // boolean
+var results = undefined // undefined
+var email = null // null
+
+
+// var num = 10
+// console.log(--num)
+// console.log(num)
+
+
+
+
+// var gender = "female";
+
+// if (gender === "male") {
+//     console.log("ALLOW")
+// } else {
+//     console.log("NOT ALLOW")
+// }
+
+
+// var age = 100
+
+// if (age >= 18 || age <= 50) {
+//     console.log("ALLOW")
+// } else {
+//     console.log("not ALLOW")
+// }
+
+// var age = +prompt("Enter your age")
+
+// if (age >= 0 && age <= 17) {
+//     console.log("KIDS")
+// } else if (age >= 18 && age <= 30) {
+//     console.log("legend")
+// }
+// else if (age >= 31 && age <= 40) {
+//     console.log("extra legend")
+// }
+// else if (age >= 41 && age <= 60) {
+//     console.log("extra extra legend")
+// } else {
+//     console.log("kya kr rhy ho duniya main abhi b")
+// }
+
+
+
+
+
+
+
+// arrays
+// var arr = ["Apple", "mango"]
+// console.log(arr[0])
+// console.log(arr[1])
+
+// var arr = []
+// arr[0] = "Apple";
+// arr[1] = "Mango";
+// arr.length = 0
+// console.log(arr)
+
+
+// arr.push("orange")
+// // arr.push("orange") //add value last index
+// // arr.pop() // remove value last index
+
+// // arr.shift() // remove first index
+// arr.unshift("mango") // add first index
+// console.log("arr", arr)
+
+// var arr = ["apple", "mango"];
+// arr.splice(1, 1, "Orange", "karachi", "lahore")
+// console.log("arr", arr)
+
+// // var copy = arr.slice(1, 2)
+// // console.log("copy", copy)
+// // console.log("arr", arr)
+// // var copy = arr.slice(0, -2)
+// var copy = arr.slice(-3)
+// console.log("copy", copy)
+
+
+// var arr = ["lahore", "", "Multan"]
+// var isMatch = false
+// for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === "karachi") {
+//         isMatch = true
+//         alert("match")
+//         break
+//     }
+// }
+
+// if (isMatch == false) {
+//     console.log("not match")
+// }
+
+
+
+
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+
+var arr = [[10, 20, 30], [40, 50, 60],]
+
+for (var i = 0; i < arr.length; i++) {
+
+    for (var j = 0; j < arr[i].length; j++) {
+        console.log(arr[i][j])
+    }
+
+}
+
+
+// console.log(arr[0][1])
+// console.log(arr[1][2])
+
+
+
+
+
+
+
+
+
+
+
+
+
