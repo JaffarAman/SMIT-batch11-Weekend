7b08e7df7dfd9d274b533792ba5ba9f6186d84ab
Jaffar Aman <amanjaffar50@gmail.com>
2024-06-29 16:40:41 +0500

    advance js

---
+++ Advance Javascript/class 01/app.js
@@ -0,0 +1,216 @@
+// ES5
+// var firstName = "Jaffar"
+// console.log("first name" , firstName)
+
+
+// ES6
+// let
+// const
+
+
+
+// var firstName = "Jaffar"
+
+// console.log(firstName)
+
+
+
+// var firstName = "Aman"
+
+
+// let
+
+// let firstName = "Jaffar"
+
+// firstName = "Aman"
+
+
+
+// console.log("firstName", firstName)
+
+
+
+
+// let name;
+// let name  = "Jaffar"
+// console.log(name)
+
+
+// // const
+// const appName = "SMIT"
+// appName = "SMIT HELLO"
+// console.log(appName, "appName")
+
+// const fullName = 0;
+// console.log("fullName", fullName)
+
+
+// block scope variable , let , const
+
+
+// if( true ){
+
+// }
+
+
+// for(){
+
+// }
+
+// function (){
+
+// }
+
+
+// let age = 23
+// let fullName;
+// if (true) {
+//     fullName = "Jaffar"
+// }
+// console.log(fullName, "fullName")
+
+
+
+
+
+
+// function foo(name){
+//     console.log("foo" , name)
+// }
+
+// foo("jaffar")
+
+// Arrow Function
+
+
+// const foo = name => {
+//     console.log("foo arrow function", name, lastName)
+// }
+
+
+// const foo = (name, lastName) => {
+//     console.log("foo arrow function", name, lastName)
+// }
+
+// foo("jaffar", "aman")
+
+
+
+
+
+
+// const add = (num1, num2) => {
+//     console.log(num1 + num2)
+// }
+
+
+// add(20, 30)
+
+
+// const foo = _ => {
+//     console.log("hello world")
+// }
+
+// foo()
+
+
+
+
+// function add() {
+//     return 10 + 20
+// }
+
+// var result = add()
+// console.log(result)
+
+
+// const add = () => {
+//     return 10 +20
+// }
+// const add = () => 10 + 30
+
+// console.log(add())
+
+
+
+
+
+// function add(num1, num2) {
+//     return num1 + num2
+// }
+
+
+// console.log(add(30, 40))
+
+
+// const add = (num1, num2) => num1 + num2
+// const add = (num1, num2) => {
+
+// }
+
+
+
+// console.log(add(500, 100))
+
+
+
+
+// this
+
+
+// console.log(this)
+
+
+
+// const obj = {
+//     firstName: "Jaffar",
+//     lastName: "Aman",
+//     fullName: function () {
+//         console.log(this)
+//     }
+// }
+
+// const obj = {
+//     firstName: "Jaffar",
+//     lastName: "Aman",
+//     fullName: () => {
+//         console.log(this)
+//     }
+// }
+
+// obj.fullName()
+
+
+
+// Object || Array =>  Destructuring
+
+
+// const stdObj = {
+//     name: "Jaffar",
+//     age: 23,
+// }
+
+// const { age, name } = stdObj
+
+
+// console.log(fullName)
+
+
+const arr = ["karachi", "lahore"]
+const [city1, city2] = arr
+console.log(city1)
+
+
+// console.log(stdObj.age)
+// var age = stdObj.age
+
+// console.log(age)
+
+
+
+
+
+
+
+
+
---
+++ JavaScript/class 30/app.js
@@ -0,0 +1,23 @@
+// console.log(window)
+
+// window.alert("HELLO WORLD")
+
+
+function login() {
+    console.log("login()")
+
+    // var isMatch = true
+
+    // if (isMatch) {
+    //     // navigate home page to about page
+    //     // window.location.href = "./about.html"
+    //     // window.location.assign("./about.html")
+    //     // window.location.replace("./about.html")
+    //     window.location.reload()
+    // } else {
+    //     alert("email or password inCorrect")
+    // }
+    window.open("./about.html" , "win1" , "width=250;height=250")
+
+
+}
