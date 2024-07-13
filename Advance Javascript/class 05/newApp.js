e51e4413c942e9c86bcd44c0e4b946bd553c0c4a
Jaffar Aman <amanjaffar50@gmail.com>
2024-07-13 16:53:49 +0500

    advance js complete

---
+++ Advance Javascript/class 05/newApp.js
@@ -0,0 +1,19 @@
+// console.log("HELLO WORLD NEW APP")
+
+// console.log("userName new" ,userName)
+
+
+
+// const userEmail = "jaffar@gmail.com"
+
+// export default userEmail
+
+const userName = "Jaffar"
+const userAge = 23
+const userEmail = "Jaffar@gmail.com"
+// export default aksjdlkajd
+export {
+    userAge,
+    userName,
+    userEmail
+}
